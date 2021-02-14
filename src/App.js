7a7bf620c3d1313e14ea84c938d73317ee2e4ba8
Rohit Sangha <1RohitSangha@gmail.com>
2021-02-13 22:56:24 -0600

    Polygons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import '@elastic/eui/dist/eui_theme_light.css';
 import styles from"./App.module.css";
 
 import { Header, Superselect, Modal} from './components';
 
-import {Map, TileLayer, Marker, Popup, Polygon} from "react-leaflet";
+import {Map, TileLayer, Marker, Popup, Polygon, GeoJSON} from "react-leaflet";
 
 import { Icon } from 'leaflet';
 
+import axios from 'axios';
+
+import {getWards} from './services/apicalls.js'
 
 
 import {
@@ -21,14 +24,39 @@ import {
     EuiForm,
     EuiCallOut,
     EuiModal,
+    EuiQuickSelectPopover,
   } from '@elastic/eui';
-
+import { get } from 'http';
 
 const App = () => {
 
     //Store States
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [features,setFeatures] = useState([]);
 
+    useEffect(() => {
+          getWards()
+          .then(features => {
+            setFeatures(features.payload)
+          })
+      }, [])
+
+
+    // getWards().then(data => {console.log(data.payload[0].geometry.coordinates[0].map(reverse))})
+
+
+
+
+      const reverse = (data) => {
+        for(let i = 0; i < data.length; i++) {
+            let lat = data[i][1];
+            let lng =  data[i][0];
+            data[i][0] =  lat;
+            data[i][1] =  lng;
+        }
+        return data;
+    }
+    
     //Api keys **move to env file
     const mapKey = '7YdX4yPR0NXL8R9yqDyX';
 
@@ -40,7 +68,13 @@ const App = () => {
     const closeModal = () => setIsModalVisible(false);
     const showModal = () => setIsModalVisible(true);
 
+
+    let wardStyle = {
+        fillColor: 'blue',
+        fillOpacity: '0.8',
     
+    }
+
     return (
     <>
     {/* Using elastic page,sidebar and header modules */}
@@ -49,24 +83,18 @@ const App = () => {
         <EuiPage>
 
             <EuiPageSideBar>
-
-               
-             
-
             </EuiPageSideBar>
 
             <EuiPageBody component="div">
                 <Modal state={isModalVisible} close={closeModal}/>
                 <EuiPageContent>
                     {/* Importing leaflet map and baselayer */}
-                    <Map className={styles.leaflet} center={center} zoom={11} scrollWheelZoom={false} zoomSnap={0}>
-                        <TileLayer url={tilesUrl} tileSize={512} crossOrigin="true" minZoom={8} zoomOffset={-1}/>
+                    <Map className={styles.leaflet} center={center} zoom={11} scrollWheelZoom={true} zoomSnap={0} maxZoom={100}>
+                        <TileLayer url={tilesUrl} tileSize={512} crossOrigin="true" minZoom={6} zoomOffset={-1}/>
                         <Marker position={[51.0,-114]} onclick={showModal}></Marker>
-
+                        {features.map(data => (<Polygon positions={data.geometry.coordinates[0].map(reverse)} opacity="1" ></Polygon>))}
+                        {/* <GeoJSON style={wardStyle} data={wards.features} /> */}
                     </Map>
-
-    
-
                 </EuiPageContent>
 
             </EuiPageBody>
---
+++ src/services/apicalls.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export async function getWards() {
+
+    let wards = await fetch('https://data.calgary.ca/resource/tz8z-hyaz.geojson', { method : 'GET'})
+      .then((response) => response.json())
+      .then(data => {
+        return data['features'];
+    });
+    
+    return {
+        type: 'GET_AGENT_DETAILS',
+        payload: wards
+      }
+}
