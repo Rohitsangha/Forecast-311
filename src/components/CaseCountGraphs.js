bb063416fc3b4b3c72bbea43b0e0e1fbe615041c
Eugene Lok <eugeneywlok@gmail.com>
2021-02-14 12:19:48 -0700

    Display case types on bar series
    - Fixed CSS formatting
    - Converted .csv to local .json
    - Added jquery
    - Removed unnecessary files/folders

--- src/components/CaseCountGraphs.js
+++ src/components/CaseCountGraphs.js
@@ -4,6 +4,7 @@ import {
     FlexibleWidthXYPlot,
     ArcSeries,
     BarSeries,
+    HorizontalBarSeries,
     VerticalBarSeries, 
     VerticalGridLines, 
     HorizontalGridLines,
@@ -11,9 +12,10 @@ import {
     YAxis,
     ChartLabel
 } from 'react-vis';
-import * as d3 from 'd3';
 import "./chart.css";
 
+
+/*
 d3.csv("count1.csv", function(d) {
     console.log(d);
     return {
@@ -23,21 +25,196 @@ d3.csv("count1.csv", function(d) {
     }).then(function(data) {
     console.log(data[0]);
 });
+*/
+
+const countData = [
+    {
+        "agency_responsible": "AO - Calgary Parking Authority",
+        "count": 359
+    },
+    {
+        "agency_responsible": "Affiliated Organizations",
+        "count": 129
+    },
+    {
+        "agency_responsible": "CFOD - Assessment",
+        "count": 3060
+    },
+    {
+        "agency_responsible": "CFOD - Customer Services and Communications",
+        "count": 58640
+    },
+    {
+        "agency_responsible": "CFOD - Finance",
+        "count": 342364
+    },
+    {
+        "agency_responsible": "CFOD - Human Resources",
+        "count": 3311
+    },
+    {
+        "agency_responsible": "CFOD - Information Technology",
+        "count": 97
+    },
+    {
+        "agency_responsible": "CS - Calgary Community Standards",
+        "count": 699939
+    },
+    {
+        "agency_responsible": "CS - Calgary Emergency Management Agency",
+        "count": 1905
+    },
+    {
+        "agency_responsible": "CS - Calgary Fire",
+        "count": 90762
+    },
+    {
+        "agency_responsible": "CS - Calgary Housing",
+        "count": 326
+    },
+    {
+        "agency_responsible": "CS - Calgary Neighbourhoods",
+        "count": 39708
+    },
+    {
+        "agency_responsible": "CS - Calgary Parks",
+        "count": 215971
+    },
+    {
+        "agency_responsible": "CS - Calgary Recreation",
+        "count": 49097
+    },
+    {
+        "agency_responsible": "Corporate Wide Service Requests",
+        "count": 19116
+    },
+    {
+        "agency_responsible": "DCMO - Corporate Analytics and Innovation",
+        "count": 13057
+    },
+    {
+        "agency_responsible": "DCMO - Facility Management",
+        "count": 1355
+    },
+    {
+        "agency_responsible": "DCMO - Fleet Services",
+        "count": 422
+    },
+    {
+        "agency_responsible": "DCMO - Real Estate and Development Services",
+        "count": 8687
+    },
+    {
+        "agency_responsible": "DCMO - Supply",
+        "count": 368
+    },
+    {
+        "agency_responsible": "Elected Officials",
+        "count": 5678
+    },
+    {
+        "agency_responsible": "Environmental and Safety Management",
+        "count": 5
+    },
+    {
+        "agency_responsible": "LL - City Clerk's",
+        "count": 10114
+    },
+    {
+        "agency_responsible": "LL - City Manager's Office",
+        "count": 266
+    },
+    {
+        "agency_responsible": "LL - Law",
+        "count": 12474
+    },
+    {
+        "agency_responsible": "Office of the City Auditor",
+        "count": 25
+    },
+    {
+        "agency_responsible": "PD - Calgary Approvals Coordination",
+        "count": 25
+    },
+    {
+        "agency_responsible": "PD - Calgary Building Services",
+        "count": 535911
+    },
+    {
+        "agency_responsible": "PD - Calgary Growth Strategies",
+        "count": 160
+    },
+    {
+        "agency_responsible": "PD - Community Planning",
+        "count": 1124
+    },
+    {
+        "agency_responsible": "PD - Emergency Response",
+        "count": 26
+    },
+    {
+        "agency_responsible": "TRAN - Calgary Transit",
+        "count": 103854
+    },
+    {
+        "agency_responsible": "TRAN - Green Line",
+        "count": 213
+    },
+    {
+        "agency_responsible": "TRAN - Roads",
+        "count": 605157
+    },
+    {
+        "agency_responsible": "TRAN - Transportation Infrastructure",
+        "count": 6260
+    },
+    {
+        "agency_responsible": "TRAN - Transportation Planning",
+        "count": 12135
+    },
+    {
+        "agency_responsible": "UEP - Environmental and Safety Management",
+        "count": 1693
+    },
+    {
+        "agency_responsible": "UEP - Waste and Recycling Services",
+        "count": 341222
+    },
+    {
+        "agency_responsible": "UEP - Water Resources",
+        "count": 19420
+    },
+    {
+        "agency_responsible": "UEP - Water Services",
+        "count": 264343
+    },
+    {
+        "agency_responsible": "US - Urban Strategy",
+        "count": 65
+    }
+];
+
+const barData = countData.map((el) => ({x: el.count, y: el.agency_responsible}));
+console.log(barData);
 
 /* eslint-disable */
 const CaseCountGraphs = () => {
-    
 
     const myData = [
-        {x: 'A', y: 10},
-        {x: 'B', y: 5},
-        {x: 'C', y: 15}
+        {x: 10, y: 'A'},
+        {x: 5, y: 'B'},
+        {x: 15, y: 'C'}
     ];
 
     return (
         <FlexibleWidthXYPlot 
-            height={400}
-            xType = "ordinal"
+            margin= {
+                {
+                    left: 300
+                }
+            }
+            height={720}
+            yType = "ordinal"
         >
         <XAxis/>
         <ChartLabel
@@ -58,12 +235,12 @@ const CaseCountGraphs = () => {
         }}
         />
         <YAxis/>
-        <VerticalBarSeries 
-            data={myData}
+        <HorizontalBarSeries 
+            data={barData}
             color='#000000'
             opacity={1}
         />
-        <HorizontalGridLines />
+        <VerticalGridLines/>
         </FlexibleWidthXYPlot>  
     )
 }
